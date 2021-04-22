983e8744ed8fcb6fad4d15f61fed8405a8e72238
RichardKT88 <kendy_t88@hotmail.com>
2021-04-22 17:24:11 -0300

    Adding Character model, controller,  enum, service and service response

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Models;
+using dotnet_rpg.Services.CharacterService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private readonly ICharacterService _characterService;
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+
+        }
+        [HttpGet]
+        [Route("GetAll")]
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> Get()
+        {
+            return Ok(await _characterService.GetAllCharacters());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ServiceResponse<Character>>> GetSingle(int id)
+        {
+            return Ok(await _characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> AddCharacter(Character newCharacter)
+        {
+            return Ok(await _characterService.AddCharacter(newCharacter));
+        }
+    }
+}
