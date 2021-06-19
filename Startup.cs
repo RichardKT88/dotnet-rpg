cde888ac34ba2fb63c8e23d2751d4f57150780d5
Kendysan88 <kendysan@gmail.com>
2021-06-19 16:47:54 -0300

    Fight And Attack with Weapons

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using dotnet_rpg.Data;
 using dotnet_rpg.Services.CharacterService;
+using dotnet_rpg.Services.FightService;
 using dotnet_rpg.Services.WeaponService;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -59,6 +60,8 @@ namespace dotnet_rpg
                 });
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IWeaponService, WeaponService>();
+            services.AddScoped<IFightService, FightService>();
+
 
         }
 
